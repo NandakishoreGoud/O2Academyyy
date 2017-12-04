72d46e149cc6f1df7bfdfd8f25055c46e2b84cf6
NandakishoreGoud <NandakishoreGoud.Doosari@cognizant.com>
2017-12-04 12:13:23 +0530

    mycode

--- src/main/java/com/o2/config/SpringConfiguration.java
+++ src/main/java/com/o2/config/SpringConfiguration.java
@@ -1,8 +1,12 @@
 package com.o2.config;
 
+import com.mongodb.MongoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -14,7 +18,20 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.o2")
 public class SpringConfiguration extends WebMvcConfigurerAdapter{
-	@Bean
+
+    @Bean
+    public MongoDbFactory mongoDbFactory() throws Exception {
+        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        return new SimpleMongoDbFactory(mongoClient, "O2Academy");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+        return mongoTemplate;
+    }
+
+    @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setViewClass(JstlView.class);
